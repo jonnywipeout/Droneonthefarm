3a338a98b743f1917f89c4aff21d56bc943e0fde
jonnywipeout <jonnywipeout@aol.com>
2025-06-21 21:37:09 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // ✅ THIS enables static export mode
+  trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
